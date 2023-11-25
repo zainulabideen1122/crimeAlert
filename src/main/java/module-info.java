6fbd5c22a4c3eaccf7d7b1a157dcacf535247f6a
Zain <zainnisarmughal@gmail.com>
2023-11-25 19:54:43 +0500

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.example.crimealert {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires java.desktop;
 
     opens com.example.crimealert to javafx.fxml;
 //    exports com.example.crimealert;
