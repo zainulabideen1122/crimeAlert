1b003581a6497949a9c0d2e3bf619b6e817e5b72
Zain <zainnisarmughal@gmail.com>
2023-11-25 23:30:35 +0500

    Adding files

--- src/main/java/com/example/crimealert/controllor/LoginController.java
+++ src/main/java/com/example/crimealert/controllor/LoginController.java
@@ -1,14 +1,43 @@
 package com.example.crimealert.controllor;
 
+import com.example.crimealert.businessLayer.LoginService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+
+
+    private LoginService loginService = new LoginService();
+
+    @FXML
+    protected void onLoginButtonClick() {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+
+        if (loginService.authenticateUser(email, password)) {
+            // Navigate to the next window or perform other actions on successful login
+//            NavigationUtil.navigateToNextWindow("next");
+            System.out.println("Login successful!");
+        } else {
+//            statusLabel.setText("Invalid email or password");
+            System.out.println("Login Unsuccessful!");
+        }
+    }
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
 }
